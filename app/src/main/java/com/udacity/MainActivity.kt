f84e70b12cc0786c7f287fd2e0765565c5f6f2ba
Wayno1979 <waynecockram@gmail.com>
2022-02-08 12:04:04 +0000

    wip - radio buttons and download button

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -9,6 +9,9 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,6 +20,8 @@ import kotlinx.android.synthetic.main.content_main.*
 class MainActivity : AppCompatActivity() {
 
     private var downloadID: Long = 0
+    private var url: String? = null
+    private var fileName: String? = null
 
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
@@ -41,24 +46,50 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun download() {
-        val request =
-            DownloadManager.Request(Uri.parse(URL))
+        if (url != null) {
+            // radio button selected, try to download
+            val request = DownloadManager.Request(Uri.parse(url))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
                 .setAllowedOverMetered(true)
                 .setAllowedOverRoaming(true)
 
-        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        // enqueue puts the download request in the queue.
-        downloadID = downloadManager.enqueue(request)
+            val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            // enqueue puts the download request in the queue.
+            downloadID = downloadManager.enqueue(request)
+        } else {
+            // no radio button selected
+            Toast.makeText(
+                applicationContext,
+                getString(R.string.select_radio_button),
+                Toast.LENGTH_LONG)
+                .show()
+        }
+    }
 
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+
+            val isChecked = view.isChecked
+
+            when (view.getId()) {
+                R.id.radio_glide -> if (isChecked) url = GLIDE_URL
+                R.id.radio_load_app -> if (isChecked)  url = LOAD_APP_URL
+                R.id.radio_retrofit -> if (isChecked) url = RETROFIT_URL
+            }
+
+            fileName = view.text.toString()
+        }
     }
 
     companion object {
-        private const val URL =
+        private const val GLIDE_URL =
+            "https://github.com/bumptech/glide/archive/refs/heads/master.zip"
+        private const val LOAD_APP_URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val RETROFIT_URL =
+            "https://github.com/square/retrofit/archive/refs/heads/master.zip"
         private const val CHANNEL_ID = "channelId"
     }
-
 }
