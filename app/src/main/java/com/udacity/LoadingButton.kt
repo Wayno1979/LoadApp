4cc114c5460b586a58549c9b32b02c94d793b15b
Wayno1979 <waynecockram@gmail.com>
2022-02-08 17:01:09 +0000

    wip - animating button

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -8,6 +8,7 @@ import android.graphics.Paint
 import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
@@ -18,6 +19,9 @@ class LoadingButton @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
 
     private var text = ""
+    private var backgroundColour = 0
+    private var loadingColour = 0
+    private var textColor = 0
     private var progress = 0
     private var widthSize = 0
     private var heightSize = 0
@@ -51,10 +55,33 @@ class LoadingButton @JvmOverloads constructor(
 
     init {
         text = resources.getString(R.string.download)
+
+        context.withStyledAttributes(attrs, R.styleable.DownloadButton) {
+            backgroundColour = getColor(R.styleable.DownloadButton_backgroundColour, 0)
+            loadingColour = getColor(R.styleable.DownloadButton_loadingColour, 0)
+            textColor = getColor(R.styleable.DownloadButton_textColour, 0)
+        }
+
+        valueAnimator.apply {
+            addUpdateListener {
+                progress = it.animatedValue as Int
+                invalidate()
+            }
+            repeatCount = ValueAnimator.INFINITE
+            repeatMode = ValueAnimator.RESTART
+        }
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+
+        // background
+        paint.color = backgroundColour
+        canvas?.drawRect(0f,0f,widthSize.toFloat(), heightSize.toFloat(), paint)
+
+        // loading
+        paint.color = loadingColour
+        canvas?.drawRect(0f, 0f, widthSize * progress/360f, heightSize.toFloat(), paint)
 
         // get x and y centre for drawing text on button
         val bounds = Rect()
@@ -63,6 +90,7 @@ class LoadingButton @JvmOverloads constructor(
         val buttonCentreX = widthSize.toFloat() / 2
         val buttonCentreY = heightSize.toFloat() / 2 - bounds.centerY()
 
+        paint.color = textColor
         canvas?.drawText(text, buttonCentreX, buttonCentreY, paint)
     }
 
